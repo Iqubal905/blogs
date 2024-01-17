c122ef6e3db1519b25e69fe11ed342c95b22cf5a
Iqubal905 <iqubal905@gmail.com>
2024-01-17 23:51:51 +0600

    'add

--- src/utilis/getSingleBlog.js
+++ src/utilis/getSingleBlog.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import getCategories from './getCategories';
 
 const getSingleBlog =async (id) => {
-    const res = await fetch(`/api/blogs/${id}`,{
-        cache : 'no-cache'
-    })
-    return res.json()
+    // const res = await fetch(`http://localhost:3000/api/blogs/${id}`,{
+    //     cache : 'no-cache'
+    // })
+    //return res.json()
+
+    const dataFromDb = await getCategories()
+    const products = dataFromDb.find((comment)=> comment._id == id )
+    const plainObject = JSON.parse(JSON.stringify(products));
+    return plainObject
+  
 };
 
 export default getSingleBlog;
