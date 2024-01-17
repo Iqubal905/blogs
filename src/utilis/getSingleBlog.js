f8b6ab19d4e2cd71d5e17a247164f2c3b5cbdf2e
Iqubal905 <iqubal905@gmail.com>
2024-01-17 23:15:05 +0600

    'add

--- src/utilis/getSingleBlog.js
+++ src/utilis/getSingleBlog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const getSingleBlog =async (id) => {
-    const res = await fetch(`http://localhost:3000/api/blogs/${id}`,{
+    const res = await fetch(`/api/blogs/${id}`,{
         cache : 'no-cache'
     })
     return res.json()
